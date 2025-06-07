391f03881760702862738fa8498bfb737a758d25
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-06-07 21:04:34 +0300

    Updating Items and ItemUnits

--- WarehouseFlow/formItem.Designer.cs
+++ WarehouseFlow/formItem.Designer.cs
@@ -34,21 +34,30 @@
             btnDelete = new Button();
             label1 = new Label();
             txtName = new TextBox();
+            label2 = new Label();
+            txtUnit = new TextBox();
+            btnAddUnit = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            dataGridView2 = new DataGridView();
+            btnUpdateUnit = new Button();
+            btnDeleteUnit = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(382, 12);
+            dataGridView1.Location = new Point(382, 53);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(406, 340);
+            dataGridView1.Size = new Size(406, 190);
             dataGridView1.TabIndex = 0;
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(5, 323);
+            btnAdd.Location = new Point(42, 137);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(94, 29);
             btnAdd.TabIndex = 1;
@@ -58,7 +67,7 @@
             // 
             // btnUpdate
             // 
-            btnUpdate.Location = new Point(135, 323);
+            btnUpdate.Location = new Point(148, 137);
             btnUpdate.Name = "btnUpdate";
             btnUpdate.Size = new Size(94, 29);
             btnUpdate.TabIndex = 2;
@@ -68,7 +77,7 @@
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(265, 323);
+            btnDelete.Location = new Point(254, 137);
             btnDelete.Name = "btnDelete";
             btnDelete.Size = new Size(94, 29);
             btnDelete.TabIndex = 3;
@@ -92,11 +101,92 @@
             txtName.Size = new Size(307, 27);
             txtName.TabIndex = 5;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Item's Unit";
+            // 
+            // txtUnit
+            // 
+            txtUnit.Location = new Point(41, 246);
+            txtUnit.Name = "txtUnit";
+            txtUnit.Size = new Size(302, 27);
+            txtUnit.TabIndex = 7;
+            // 
+            // btnAddUnit
+            // 
+            btnAddUnit.Location = new Point(41, 293);
+            btnAddUnit.Name = "btnAddUnit";
+            btnAddUnit.Size = new Size(94, 29);
+            btnAddUnit.TabIndex = 8;
+            btnAddUnit.Text = "Add Unit";
+            btnAddUnit.UseVisualStyleBackColor = true;
+            btnAddUnit.Click += btnAddUnit_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(382, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Items";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(382, 249);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Item Unit";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(382, 275);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(406, 78);
+            dataGridView2.TabIndex = 11;
+            // 
+            // btnUpdateUnit
+            // 
+            btnUpdateUnit.Location = new Point(141, 293);
+            btnUpdateUnit.Name = "btnUpdateUnit";
+            btnUpdateUnit.Size = new Size(101, 29);
+            btnUpdateUnit.TabIndex = 12;
+            btnUpdateUnit.Text = "Update Unit";
+            btnUpdateUnit.UseVisualStyleBackColor = true;
+            btnUpdateUnit.Click += button1_Click;
+            // 
+            // btnDeleteUnit
+            // 
+            btnDeleteUnit.Location = new Point(248, 293);
+            btnDeleteUnit.Name = "btnDeleteUnit";
+            btnDeleteUnit.Size = new Size(94, 29);
+            btnDeleteUnit.TabIndex = 13;
+            btnDeleteUnit.Text = "Delete Unit";
+            btnDeleteUnit.UseVisualStyleBackColor = true;
+            btnDeleteUnit.Click += btnDeleteUnit_Click;
+            // 
             // formItem
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 365);
+            Controls.Add(btnDeleteUnit);
+            Controls.Add(btnUpdateUnit);
+            Controls.Add(dataGridView2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(btnAddUnit);
+            Controls.Add(txtUnit);
+            Controls.Add(label2);
             Controls.Add(txtName);
             Controls.Add(label1);
             Controls.Add(btnDelete);
@@ -105,7 +195,9 @@
             Controls.Add(dataGridView1);
             Name = "formItem";
             Text = "ItemForm";
+            Load += formItem_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -118,5 +210,13 @@
         private Button btnDelete;
         private Label label1;
         private TextBox txtName;
+        private Label label2;
+        private TextBox txtUnit;
+        private Button btnAddUnit;
+        private Label label3;
+        private Label label4;
+        private DataGridView dataGridView2;
+        private Button btnUpdateUnit;
+        private Button btnDeleteUnit;
     }
 }
--- WarehouseFlow/formItem.cs
+++ WarehouseFlow/formItem.cs
@@ -20,6 +20,8 @@ namespace WarehouseFlow
             InitializeComponent();
             LoadItems();
             dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+            dataGridView2.SelectionChanged += dataGridView2_SelectionChanged;
+
         }
 
         private void LoadItems()
@@ -27,12 +29,17 @@ namespace WarehouseFlow
             dataGridView1.DataSource = _context.Items
                 .Select(i => new { i.Id, i.Name })
                 .ToList();
+            //dataGridView2.DataSource = _context.ItemUnits
+            //    .Where(i => i.ItemId == (int)dataGridView1.CurrentRow.Cells["Id"].Value)
+            //    .Select(i => new { i.ItemId, i.Unit })
+            //    .ToList();
             dataGridView1.Columns["Id"].Visible = false;
+            //dataGridView2.Columns["ItemId"].Visible = false;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+
             var item = new Item { Name = txtName.Text };
             _context.Items.Add(item);
             _context.SaveChanges();
@@ -42,7 +49,7 @@ namespace WarehouseFlow
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            
+
             if (dataGridView1.CurrentRow != null)
             {
                 int id = (int)dataGridView1.CurrentRow.Cells["Id"].Value;
@@ -59,7 +66,7 @@ namespace WarehouseFlow
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            
+
             if (dataGridView1.CurrentRow != null)
             {
                 int id = (int)dataGridView1.CurrentRow.Cells["Id"].Value;
@@ -79,6 +86,71 @@ namespace WarehouseFlow
             if (dataGridView1.CurrentRow != null)
             {
                 txtName.Text = dataGridView1.CurrentRow.Cells["Name"].Value?.ToString();
+                dataGridView2.DataSource = _context.ItemUnits
+                    .Where(i => i.ItemId == (int)dataGridView1.CurrentRow.Cells["Id"].Value)
+                    .Select(i => new { i.ItemId, i.Unit }).ToList();
+                dataGridView2.Columns["ItemId"].Visible = false;
+            }
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentRow != null)
+            {
+                txtUnit.Text = dataGridView2.CurrentRow.Cells["Unit"].Value?.ToString();
+            }
+        }
+
+        private void formItem_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddUnit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int id = (int)dataGridView1.CurrentRow.Cells["Id"].Value;
+                var itemunit = new ItemUnit { ItemId = id, Unit = txtUnit.Text };
+                _context.ItemUnits.Add(itemunit);
+                _context.SaveChanges();
+                LoadItems();
+                txtUnit.Clear();
+            }
+        }
+
+        //update item unit
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentRow != null)
+            {
+                int id = (int)dataGridView2.CurrentRow.Cells["ItemId"].Value;
+                var itemUnit = _context.ItemUnits.Find(id, (string)dataGridView2.CurrentRow.Cells["Unit"].Value);
+                if (itemUnit != null)
+                {
+                    _context.ItemUnits.Remove(itemUnit);
+                    _context.SaveChanges();
+                    _context.ItemUnits.Add(new ItemUnit { ItemId = id, Unit = txtUnit.Text});
+                    _context.SaveChanges();
+                    LoadItems();
+                    txtUnit.Clear();
+                }
+            }
+        }
+
+        private void btnDeleteUnit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentRow != null)
+            {
+                int id = (int)dataGridView2.CurrentRow.Cells["ItemId"].Value;
+                var itemUnit = _context.ItemUnits.Find(id, (string)dataGridView2.CurrentRow.Cells["Unit"].Value);
+                if (itemUnit != null)
+                {
+                    _context.ItemUnits.Remove(itemUnit);
+                    _context.SaveChanges();
+                    LoadItems();
+                    txtUnit.Clear();
+                }
             }
         }
     }
