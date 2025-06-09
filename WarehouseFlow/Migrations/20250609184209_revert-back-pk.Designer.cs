9e82bb8cc2877614136352804a4c4eec1c76ef52
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-06-09 21:42:53 +0300

    reverting the migration

--- WarehouseFlow/Migrations/20250609184209_revert-back-pk.Designer.cs
+++ WarehouseFlow/Migrations/20250609184209_revert-back-pk.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarehouseFlow;
 
@@ -11,9 +12,11 @@ using WarehouseFlow;
 namespace WarehouseFlow.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250609184209_revert-back-pk")]
+    partial class revertbackpk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WarehouseFlow/WarehouseItem.cs
+++ WarehouseFlow/WarehouseItem.cs
@@ -9,19 +9,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WarehouseFlow
 {
-    //[PrimaryKey(nameof(ItemId), nameof(ProductionDate), nameof(ShelfLife))]
+    [PrimaryKey(nameof(ItemId), nameof(ProductionDate), nameof(ShelfLife))]
     public class WarehouseItem
     {
-        [Key]
-        public int ID { get; set; }
-
-        public int ItemId { get; set; }
-        public DateTime ProductionDate { get; set; }
-        public int ShelfLife { get; set; }
-
-        //[Column(Order = 0)] public int ItemId { get; set; }
-        //[Column(Order = 1)] public DateTime ProductionDate { get; set; }
-        //[Column(Order = 2)] public int ShelfLife { get; set; }
+        [Column(Order = 0)] public int ItemId { get; set; }
+        [Column(Order = 1)] public DateTime ProductionDate { get; set; }
+        [Column(Order = 2)] public int ShelfLife { get; set; }
 
         public int WarehouseId { get; set; }
         public int SupplierId { get; set; }
